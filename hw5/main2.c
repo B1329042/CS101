a5b3edf0cd2d36d3c2f35abd5e4ec3da2ed992b5
B1329042 <l25365686@gmail.com>
2024-12-05 19:15:08 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    for (int n = 1; n <= 81; n++) {
-        int i = (n - 1) / 9 + 1;   
-        int j = (n - 1) % 9 + 1;   
-        printf("%d x %d = %2d\t", i, j, i * j);
-        if (j == 9) {
-            printf("\n");
-        }
-    }
-    return 0;
-}
 
