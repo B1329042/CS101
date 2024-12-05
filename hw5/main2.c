d5743a4db3d5f8157cdb2c3a2c5723d888a5f3e2
B1329042 <l25365686@gmail.com>
2024-12-05 23:27:33 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
-
 int main() {
     double PI=0.0f;
     double c=4.0f;
     int b=1;
     int x=1;
     for (int b=1;PI<=3.141585;b=b+4){
-        x=x+2;
         PI=PI+(c/b);
         printf("%.5f\n",PI);
         printf("%d\n",b);
         if(PI>=3.141585&PI<=3.141594){
+            printf("%.5f\n",PI);
+            printf("%d\n",x);
             break;
         }
         x=x+2;
         PI=PI-(c/(b+2));
         printf("%.5f\n",PI);
-        printf("%d\n",b);
+        printf("%d\n",b-2);
         if(PI>=3.141585&PI<=3.141594){
+            printf("%.5f\n",PI);
+            printf("%d\n",x);
             break;
         }
+        x=x+2;
     }
     printf("%.5f\n",PI);
     printf("%d",x-4);
     return 0;
 }
-
